fe82f16ca06a8efc738e124f15a891e83d37c4d5
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-13 15:35:00 +0700

    layouting login and register page

--- Submission/StoryApp/app/src/main/java/com/piwew/storyapp/WelcomeActivity.kt
+++ Submission/StoryApp/app/src/main/java/com/piwew/storyapp/WelcomeActivity.kt
@@ -3,16 +3,19 @@ package com.piwew.storyapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.piwew.storyapp.databinding.ActivityMainBinding
+import com.piwew.storyapp.databinding.ActivityWelcomeBinding
 
-class MainActivity : AppCompatActivity() {
+class WelcomeActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
+    private lateinit var binding: ActivityWelcomeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityWelcomeBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.btnLogin.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
 
         binding.btnRegister.setOnClickListener {
             startActivity(Intent(this, RegisterActivity::class.java))
