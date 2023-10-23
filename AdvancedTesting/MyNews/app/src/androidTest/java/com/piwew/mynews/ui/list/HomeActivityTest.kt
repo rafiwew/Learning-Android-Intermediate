0d74bdbeb83cde992f77578941562eb6c1f4b687
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-23 22:43:43 +0700

    initial end-to-end testing espresso

---
+++ AdvancedTesting/MyNews/app/src/androidTest/java/com/piwew/mynews/ui/list/HomeActivityTest.kt
@@ -0,0 +1,29 @@
+package com.piwew.mynews.ui.list
+
+import androidx.test.espresso.IdlingRegistry
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.LargeTest
+import com.piwew.mynews.utils.EspressoIdlingResource
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@LargeTest
+class HomeActivityTest {
+
+    @get:Rule
+    val activity = ActivityScenarioRule(HomeActivity::class.java)
+
+    @Before
+    fun setUp() {
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.countingIdlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+    }
+}
