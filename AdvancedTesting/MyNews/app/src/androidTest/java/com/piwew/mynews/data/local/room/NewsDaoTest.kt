7a27175e58aaca80f9b5a16f7912818a046cca31
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-23 21:27:05 +0700

    finished integration test
    finished integration test for class NewsDao using in memory test

--- AdvancedTesting/MyNews/app/src/androidTest/java/com/piwew/mynews/data/local/room/NewsDaoTest.kt
+++ AdvancedTesting/MyNews/app/src/androidTest/java/com/piwew/mynews/data/local/room/NewsDaoTest.kt
@@ -3,9 +3,14 @@ package com.piwew.mynews.data.local.room
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
+import com.piwew.mynews.utils.DataDummy
+import com.piwew.mynews.utils.getOrAwaitValue
+import kotlinx.coroutines.test.runTest
 import org.junit.After
+import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 
 class NewsDaoTest {
 
@@ -14,6 +19,7 @@ class NewsDaoTest {
 
     private lateinit var database: NewsDatabase
     private lateinit var dao: NewsDao
+    private val sampleNews = DataDummy.generateDummyNewsEntity()[0]
 
     @Before
     fun initDb() {
@@ -26,4 +32,31 @@ class NewsDaoTest {
 
     @After
     fun closeDb() = database.close()
+
+    /*
+    * A. Ketika menyimpan data berhasil
+    * - Sama dengan data ketika getBookmarkedNews.
+    * - isNewsBookmarked bernilai true.
+    * */
+    @Test
+    fun saveNews_Success() = runTest {
+        dao.saveNews(sampleNews)
+        val actualNews = dao.getBookmarkedNews().getOrAwaitValue()
+        Assert.assertEquals(sampleNews.title, actualNews[0].title)
+        Assert.assertTrue(dao.isNewsBookmarked(sampleNews.title).getOrAwaitValue())
+    }
+
+    /*
+    * B. Ketika menghapus data berhasil
+    * - Data getBookmarkedNews kosong.
+    * - isNewsBookmarked bernilai false.
+    * */
+    @Test
+    fun deleteNews_Success() = runTest {
+        dao.saveNews(sampleNews)
+        dao.deleteNews(sampleNews.title)
+        val actualNews = dao.getBookmarkedNews().getOrAwaitValue()
+        Assert.assertTrue(actualNews.isEmpty())
+        Assert.assertFalse(dao.isNewsBookmarked(sampleNews.title).getOrAwaitValue())
+    }
 }
