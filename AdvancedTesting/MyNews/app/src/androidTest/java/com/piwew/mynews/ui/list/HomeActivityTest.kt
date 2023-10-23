673a561b6db3840ed150541951f55feb4083b5d6
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-23 22:54:07 +0700

    finished end-to-end testing espresso

--- AdvancedTesting/MyNews/app/src/androidTest/java/com/piwew/mynews/ui/list/HomeActivityTest.kt
+++ AdvancedTesting/MyNews/app/src/androidTest/java/com/piwew/mynews/ui/list/HomeActivityTest.kt
@@ -1,13 +1,28 @@
 package com.piwew.mynews.ui.list
 
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.Espresso.pressBack
 import androidx.test.espresso.IdlingRegistry
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.intent.Intents
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasComponent
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
+import com.piwew.mynews.R
+import com.piwew.mynews.ui.detail.NewsDetailActivity
 import com.piwew.mynews.utils.EspressoIdlingResource
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
@@ -25,5 +40,51 @@ class HomeActivityTest {
     @After
     fun tearDown() {
         IdlingRegistry.getInstance().unregister(EspressoIdlingResource.countingIdlingResource)
+    }
+
+    @Test
+    fun loadHeadlineNews_Success() {
+        onView(withId(R.id.rv_news)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_news)).perform(
+            RecyclerViewActions.scrollToPosition<RecyclerView.ViewHolder>(
+                10
+            )
+        )
+    }
+
+    @Test
+    fun loadDetailNews_Success() {
+        Intents.init()
+        onView(withId(R.id.rv_news)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        intended(hasComponent(NewsDetailActivity::class.java.name))
+        onView(withId(R.id.webView)).check(matches(isDisplayed()))
+    }
+
+    @Test
+    fun loadBookmarkedNews_Success() {
+        onView(withId(R.id.rv_news)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.action_bookmark)).perform(click())
+        pressBack()
+        onView(withText("BOOKMARK")).perform(click())
+        onView(withId(R.id.rv_news)).check(matches(isDisplayed()))
+        onView(withId(R.id.rv_news)).perform(
+            RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                0,
+                click()
+            )
+        )
+        onView(withId(R.id.webView)).check(matches(isDisplayed()))
+        onView(withId(R.id.action_bookmark)).perform(click())
+        pressBack()
     }
 }
