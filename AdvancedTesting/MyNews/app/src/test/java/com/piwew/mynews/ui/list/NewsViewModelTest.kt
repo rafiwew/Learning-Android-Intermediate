a9b81babb699fcd4f54de701cb02331f777991b0
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-23 17:05:40 +0700

    livedata unit testing

---
+++ AdvancedTesting/MyNews/app/src/test/java/com/piwew/mynews/ui/list/NewsViewModelTest.kt
@@ -0,0 +1,51 @@
+package com.piwew.mynews.ui.list
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
+import com.piwew.mynews.data.NewsRepository
+import com.piwew.mynews.data.Result
+import com.piwew.mynews.data.local.entity.NewsEntity
+import com.piwew.mynews.utils.DataDummy
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+
+@RunWith(MockitoJUnitRunner::class)
+class NewsViewModelTest {
+
+    @get:Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var newsRepository: NewsRepository
+    private lateinit var newsViewModel: NewsViewModel
+    private val dummyNews = DataDummy.generateDummyNewsEntity()
+
+    @Before
+    fun setUp() {
+        newsViewModel = NewsViewModel(newsRepository)
+    }
+
+    @Test
+    fun `when Get HeadlineNews should not null and return success`() {
+        val observer = Observer<Result<List<NewsEntity>>> {}
+        try {
+            val expectedNews = MutableLiveData<Result<List<NewsEntity>>>()
+            expectedNews.value = Result.Success(dummyNews)
+            `when`(newsRepository.getHeadlineNews()).thenReturn(expectedNews)
+            val actualNews = newsViewModel.getHeadlineNews().observeForever(observer)
+
+            Mockito.verify(newsRepository).getHeadlineNews()
+            Assert.assertNotNull(actualNews)
+        } finally {
+            newsViewModel.getHeadlineNews().removeObserver(observer)
+        }
+    }
+}
