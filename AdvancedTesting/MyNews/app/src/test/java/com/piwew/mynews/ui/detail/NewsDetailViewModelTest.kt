6ab85f205200126f59752b095545a08285d7b1c0
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-23 19:19:48 +0700

    initial unit testing coroutines

---
+++ AdvancedTesting/MyNews/app/src/test/java/com/piwew/mynews/ui/detail/NewsDetailViewModelTest.kt
@@ -0,0 +1,63 @@
+package com.piwew.mynews.ui.detail
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.piwew.mynews.data.NewsRepository
+import com.piwew.mynews.utils.DataDummy
+import com.piwew.mynews.utils.getOrAwaitValue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.TestDispatcher
+import kotlinx.coroutines.test.UnconfinedTestDispatcher
+import kotlinx.coroutines.test.resetMain
+import kotlinx.coroutines.test.runTest
+import kotlinx.coroutines.test.setMain
+import org.junit.After
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
+import org.mockito.junit.MockitoJUnitRunner
+
+@OptIn(ExperimentalCoroutinesApi::class)
+@RunWith(MockitoJUnitRunner::class)
+class NewsDetailViewModelTest {
+    @get: Rule
+    val instantExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    private lateinit var newsRepository: NewsRepository
+    private lateinit var newsDetailViewModel: NewsDetailViewModel
+    private val dummyDetailNews = DataDummy.generateDummyNewsEntity()[0]
+
+    @Before
+    fun setUp() {
+        newsDetailViewModel = NewsDetailViewModel(newsRepository)
+        newsDetailViewModel.setNewsData(dummyDetailNews)
+    }
+
+    private val testDispatcher: TestDispatcher = UnconfinedTestDispatcher()
+
+    @Before
+    fun setupDispatcher() {
+        Dispatchers.setMain(testDispatcher)
+    }
+
+    @After
+    fun tearDownDispatcher() {
+        Dispatchers.resetMain()
+    }
+
+    @Test
+    fun `when bookmarkStatus false should call saveNews`() = runTest {
+        val expectedBoolean = MutableLiveData<Boolean>()
+        expectedBoolean.value = false
+        `when`(newsRepository.isNewsBookmarked(dummyDetailNews.title)).thenReturn(expectedBoolean)
+        newsDetailViewModel.bookmarkStatus.getOrAwaitValue()
+        newsDetailViewModel.changeBookmark(dummyDetailNews)
+        Mockito.verify(newsRepository).saveNews(dummyDetailNews)
+    }
+}
