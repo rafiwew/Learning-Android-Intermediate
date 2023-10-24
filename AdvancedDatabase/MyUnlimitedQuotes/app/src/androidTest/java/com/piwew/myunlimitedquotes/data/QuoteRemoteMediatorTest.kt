4d479d57ce9ad963dd03abbb5d443f1cf398119c
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-25 04:45:15 +0700

    unit testing paging 3

---
+++ AdvancedDatabase/MyUnlimitedQuotes/app/src/androidTest/java/com/piwew/myunlimitedquotes/data/QuoteRemoteMediatorTest.kt
@@ -0,0 +1,66 @@
+package com.piwew.myunlimitedquotes.data
+
+import androidx.paging.*
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.piwew.myunlimitedquotes.database.QuoteDatabase
+import com.piwew.myunlimitedquotes.network.ApiService
+import com.piwew.myunlimitedquotes.network.QuoteResponseItem
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runTest
+import org.junit.After
+import org.junit.Assert
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalPagingApi
+@RunWith(AndroidJUnit4::class)
+class QuoteRemoteMediatorTest {
+
+    private var mockApi: ApiService = FakeApiService()
+    private var mockDb: QuoteDatabase = Room.inMemoryDatabaseBuilder(
+        ApplicationProvider.getApplicationContext(),
+        QuoteDatabase::class.java
+    ).allowMainThreadQueries().build()
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun refreshLoadReturnsSuccessResultWhenMoreDataIsPresent() = runTest {
+        val remoteMediator = QuoteRemoteMediator(
+            mockDb,
+            mockApi
+        )
+        val pagingState = PagingState<Int, QuoteResponseItem>(
+            listOf(),
+            null,
+            PagingConfig(10),
+            10
+        )
+        val result = remoteMediator.load(LoadType.REFRESH, pagingState)
+        Assert.assertTrue(result is RemoteMediator.MediatorResult.Success)
+        Assert.assertFalse((result as RemoteMediator.MediatorResult.Success).endOfPaginationReached)
+    }
+
+    @After
+    fun tearDown() {
+        mockDb.clearAllTables()
+    }
+}
+
+class FakeApiService : ApiService {
+    override suspend fun getQuote(page: Int, size: Int): List<QuoteResponseItem> {
+        val items: MutableList<QuoteResponseItem> = arrayListOf()
+
+        for (i in 0..100) {
+            val quote = QuoteResponseItem(
+                i.toString(),
+                "author + $i",
+                "quote $i"
+            )
+            items.add(quote)
+        }
+
+        return items.subList((page - 1) * size, (page - 1) * size + size)
+    }
+}
