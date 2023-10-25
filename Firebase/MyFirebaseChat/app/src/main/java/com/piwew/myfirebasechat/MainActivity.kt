d5ea8d4598e0cdfcfc3de8292ba15c65bb5ef5b0
Muhammad Rafi <mochrafi01@gmail.com>
2023-10-25 15:42:46 +0700

    firebase realtime db (chat app)

--- Firebase/MyFirebaseChat/app/src/main/java/com/piwew/myfirebasechat/MainActivity.kt
+++ Firebase/MyFirebaseChat/app/src/main/java/com/piwew/myfirebasechat/MainActivity.kt
@@ -5,15 +5,26 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.database.FirebaseRecyclerOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.FirebaseDatabase
+import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
+import com.piwew.myfirebasechat.adapter.FirebaseMessageAdapter
 import com.piwew.myfirebasechat.databinding.ActivityMainBinding
+import com.piwew.myfirebasechat.model.Message
+import java.util.Date
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var auth: FirebaseAuth
+
+    private lateinit var db: FirebaseDatabase
+    private lateinit var adapter: FirebaseMessageAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +39,52 @@ class MainActivity : AppCompatActivity() {
             finish()
             return
         }
+
+        db = Firebase.database
+
+        val messageRef = db.reference.child(MESSAGE_CHILD)
+
+        binding.sendButton.setOnClickListener {
+            val friendlyMessage = Message(
+                binding.messageEditText.text.toString(),
+                firebaseUser.displayName.toString(),
+                firebaseUser.photoUrl.toString(),
+                Date().time
+            )
+            messageRef.push().setValue(friendlyMessage) { error, _ ->
+                if (error != null) {
+                    Toast.makeText(
+                        this,
+                        getString(R.string.send_error) + error.message,
+                        Toast.LENGTH_SHORT
+                    ).show()
+                } else {
+                    Toast.makeText(this, getString(R.string.send_success), Toast.LENGTH_SHORT)
+                        .show()
+                }
+            }
+            binding.messageEditText.setText("")
+        }
+
+        val manager = LinearLayoutManager(this)
+        manager.stackFromEnd = true
+        binding.messageRecyclerView.layoutManager = manager
+
+        val options = FirebaseRecyclerOptions.Builder<Message>()
+            .setQuery(messageRef, Message::class.java)
+            .build()
+        adapter = FirebaseMessageAdapter(options, firebaseUser.displayName)
+        binding.messageRecyclerView.adapter = adapter
+    }
+
+    override fun onResume() {
+        super.onResume()
+        adapter.startListening()
+    }
+
+    override fun onPause() {
+        adapter.stopListening()
+        super.onPause()
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -51,5 +108,9 @@ class MainActivity : AppCompatActivity() {
         auth.signOut()
         startActivity(Intent(this, LoginActivity::class.java))
         finish()
+    }
+
+    companion object {
+        const val MESSAGE_CHILD = "messages"
     }
 }
---
+++ Firebase/MyFirebaseChat/app/src/main/java/com/piwew/myfirebasechat/model/Message.kt
@@ -0,0 +1,10 @@
+package com.piwew.myfirebasechat.model
+
+data class Message(
+    val text: String? = null,
+    val name: String? = null,
+    val photoUrl: String? = null,
+    val timestamp: Long? = null
+) {
+    //
+}
